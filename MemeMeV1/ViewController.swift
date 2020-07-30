59c0c4531e0f3ae8ec116302d3688d14a2f5b1bd
EM <emilharoutunian@gmail.com>
2020-07-29 20:00:24 -0700

    Initial Commit

---
+++ MemeMeV1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MemeMeV1
+//
+//  Created by Emil Haroutunian on 7/29/20.
+//  Copyright © 2020 Emil Haroutunian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
